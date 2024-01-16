bb62220eed70ec116f9639c2234190a025a99270
Lucas wu <wsx1531913990@hotmail.com>
2024-01-16 20:28:31 +0800

    Last commit

--- conftest.py
+++ conftest.py
@@ -10,16 +10,17 @@ from selenium.webdriver.support import expected_conditions as EC
 @pytest.fixture(scope="function", autouse=True)
 def driver():
     options = Options()
-    options.add_argument('--headless')
     options.add_argument('--start-maximized')
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(1)
     driver.get("https://iviewui.com")
     sleep(0.5)
     print("开始测试")
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable(By.XPATH, "//a[@href='/view-ui-plus/']")).click()
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable(By.XPATH, "//span[@class='i-layout-menu-side-title' and contains(., '组件')]")).click()
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='i-layout-menu-side-title-text' and contains(., '表单')]"))).click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[@href='/view-ui-plus/']"))).click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+        (By.XPATH, "//span[@class='i-layout-menu-side-title' and contains(., '组件')]"))).click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+        (By.XPATH, "//span[@class='i-layout-menu-side-title-text' and contains(., '表单')]"))).click()
     yield driver
     print("结束测试")
     driver.quit()
