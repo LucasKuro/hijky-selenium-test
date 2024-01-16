00de56cec22d0873e8334914381cb3008b97f3d7
Lucas wu <wsx1531913990@hotmail.com>
2024-01-16 18:43:30 +0800

    update pytest info

--- conftest.py
+++ conftest.py
@@ -3,12 +3,14 @@ from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 @pytest.fixture(scope="function", autouse=True)
 def driver():
     options = Options()
-    options.add_argument("--headless, --start-maximized")
+    options.add_argument("--start-maximized")
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(1)
     driver.get("https://iviewui.com")
@@ -17,8 +19,7 @@ def driver():
     driver.find_element(By.XPATH, "//a[@href='/view-ui-plus/']").click()
     # driver.find_element(By.XPATH, "//span[@class='i-layout-header-trigger']").click()
     driver.find_element(By.XPATH, "//span[@class='i-layout-menu-side-title' and contains(., '组件')]").click()
-    driver.find_element(By.XPATH,
-                        "//span[@class='i-layout-menu-side-title-text' and contains(., '表单')]").click()
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//span[@class='i-layout-menu-side-title-text' and contains(., '表单')]"))).click()
     yield driver
     print("结束测试")
     driver.quit()
