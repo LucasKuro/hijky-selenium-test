f2c137fe88ed601441c3ee7eb92e0099361c1a55
Lucas wu <wsx1531913990@hotmail.com>
2024-02-01 14:37:18 +0800

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -18,7 +18,8 @@ def driver():
     print("开始测试")
     WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//a[@href='/en/getting-started"
                                                                           "/installation/']"))).click()
-    driver.find_element(By.XPATH, "//div[@id='v-list-group--id-Components']").click()
+    WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//div[@id='v-list-group--id-Components' "
+                                                                          "and contains(., 'Components')]"))).click()
     yield driver
     print("结束测试")
     driver.quit()
